4bb771507152e24f353164e6762050e3d3fe3624
[pyt111] <[584702675@qq.com]>
2018-04-26 23:03:05 +0800

    12

--- hello-word/js/hello-word.js
+++ hello-word/js/hello-word.js
@@ -2,4 +2,19 @@ console.log(G);
 
 G.F.loadMain = () => {
     this.AI = G.F.mainAI;
-}
+};
+G.F.mainAI = () => {
+
+};
+G.F.shipAI = () => {
+
+};
+
+G.F.bulletAI = () => {
+
+};
+G.F.explosinoAI = (cmd) => {
+
+};
+
+G.makeBlock('main',G.F.loadMain).loadBlock('main')
